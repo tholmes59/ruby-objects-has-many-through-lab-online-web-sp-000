9211a87b05f6afea0bfd0cc01b1ba312a7ba7567
Tom Holmes <tholmes59@gmail.com>
2019-01-30 21:23:57 +0000

    Automatically backed up by Learn

--- spec/05_appointment_spec.rb
+++ spec/05_appointment_spec.rb
@@ -2,6 +2,17 @@ require "spec_helper"
 
 describe "Appointment" do
 
+  
+
+  describe "#new" do
+    it "initializes with a patient, doctor, and date" do
+      doctor_who = Doctor.new("The Doctor")
+      hevydevy = Patient.new("Devin Townsend")
+
+      expect{Appointment.new(hevydevy, doctor_who, "Friday, January 32nd")}.to_not raise_error
+    end
+  end
+  
   describe ".all" do
     it "knows about all appointments that have been created" do
       doctor_who = Doctor.new("The Doctor")
@@ -11,15 +22,6 @@ describe "Appointment" do
       doctor_who.new_appointment(hevydevy, "Satunday, January 34nd")
 
       expect(Appointment.all).to include(appointment)
-    end
-  end
-
-  describe "#new" do
-    it "initializes with a patient, doctor, and date" do
-      doctor_who = Doctor.new("The Doctor")
-      hevydevy = Patient.new("Devin Townsend")
-
-      expect{Appointment.new(hevydevy, doctor_who, "Friday, January 32nd")}.to_not raise_error
     end
   end
 
